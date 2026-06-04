9bd2fd49cb17c1e76c25308035380d100833a7b3
ljl <ljl@abc.com>
2026-06-05 00:42:58 +0800

    修复终端输入问题，美化终端输出

--- demo.py
+++ demo.py
@@ -1,1 +1,16 @@
 import os
+try:
+    import readline
+except ImportError:
+    # Windows 下可以试试 pyreadline3
+    try:
+        import pyreadline3 as readline
+    except ImportError:
+        pass
+
+
+while True:
+    text = input("用户：")
+    if text == "exit":
+        break
+    print(text)
